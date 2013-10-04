424f32b44dfbed98c97dafc81b8e43e20c6830a3
MalcyL <malcolm@landonsonline.me.uk>
2013-10-04 10:31:46 +0100

    Initial checkin - Receive and output metric to console.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+# Specify your gem's dependencies in udp2sqs_server.gemspec
+gemspec
---
+++ lib/metric_handler/metric_handler.rb
@@ -0,0 +1,56 @@
+require 'socket'
+require 'fog'
+require 'eventmachine'
+
+module MetricHandler
+
+  class MetricHandler
+
+    def initialize
+      @host, @port, @threadpool_size = "0.0.0.0", 9732, 100
+    end
+
+    def configure(host: "0.0.0.0", port: 9732, threadpool_size: 100)
+      @host, @port, @threadpool_size = host, port, threadpool_size
+    end
+
+    def run
+      EM.threadpool_size = @threadpool_size
+      EM.run do
+        warmup_threads = proc do
+          i = 0
+          i += 1
+        end
+        EM.defer(warmup_threads)
+
+        config = YAML.load_file("queue.yaml")
+
+        sqs = Fog::AWS::SQS.new(
+         :aws_access_key_id => config['access_key'],
+         :aws_secret_access_key => config['secret_key'],
+         :region => config['queue_region']
+        )
+        loop do
+          response = sqs.receive_message(config['queue_url'])
+          messages = response.body['Message']
+          if messages.empty? 
+            sleep 2
+          else
+            messages.each do |m|
+              operation = proc do
+
+                # Handle message
+
+                sqs.delete_message(config['queue_url'], m['ReceiptHandle'])
+                puts m
+              end
+              EM.defer(operation)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
